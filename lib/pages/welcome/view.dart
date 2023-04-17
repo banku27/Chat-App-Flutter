b89396dd86ae427449f9eaddd57d342a1c45ed2c
banku27 <pankajparihar123321@gmail.com>
2023-04-17 14:27:56 +0530

    Dots indicator

--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,8 +1,6 @@
 import 'package:chat_app_flutter/pages/welcome/controller.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class WelcomePage extends GetView<WelcomeController> {
@@ -11,59 +9,101 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            PageView(
-              scrollDirection: Axis.horizontal,
-              pageSnapping: true,
-              reverse: false,
-              controller: PageController(
-                initialPage: 0,
-                keepPage: false,
-                viewportFraction: 1,
+      body: Obx(
+        () => SizedBox(
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              PageView(
+                onPageChanged: (index) {
+                  controller.changePage(index);
+                },
+                scrollDirection: Axis.horizontal,
+                pageSnapping: true,
+                reverse: false,
+                controller: PageController(
+                  initialPage: 0,
+                  keepPage: false,
+                  viewportFraction: 1,
+                ),
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          'assets/images/banner1.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          'assets/images/banner2.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          'assets/images/banner3.png',
+                        ),
+                      ),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Positioned(
+                          bottom: 80,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.white),
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.black),
+                                shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ))),
+                            child: const Text('Login'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        'assets/images/banner1.png',
-                      ),
+              Positioned(
+                bottom: 50,
+                child: DotsIndicator(
+                  dotsCount: 3,
+                  decorator: DotsDecorator(
+                    size: const Size.square(9),
+                    activeSize: const Size(18, 9),
+                    activeShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
                     ),
                   ),
+                  position: controller.state.index.toDouble(),
+                  reversed: false,
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        'assets/images/banner2.png',
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(
-                        'assets/images/banner3.png',
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
