a9d08e9e1f4cb8bb1175513245911de5d3efec71
banku27 <pankajparihar123321@gmail.com>
2023-04-22 23:43:00 +0530

    sign in folder structure and controller

---
+++ lib/pages/sign-in/controller.dart
@@ -0,0 +1,7 @@
+import 'package:chat_app_flutter/pages/sign-in/state.dart';
+import 'package:get/get.dart';
+
+class SignInController extends GetxController {
+  final state = SignInState();
+  SignInController();
+}
